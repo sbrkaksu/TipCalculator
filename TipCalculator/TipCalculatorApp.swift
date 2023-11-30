120c246b0982d55ae016c917960d8af2c6400c27
Berk Aksu <berkaksu79@gmail.com>
2023-11-30 18:02:54 +0300

    Initial Commit

---
+++ TipCalculator/TipCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TipCalculatorApp.swift
+//  TipCalculator
+//
+//  Created by Berk Aksu on 30.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct TipCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
